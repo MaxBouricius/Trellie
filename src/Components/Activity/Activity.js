1ca9c7485ac134ef4a437dc0002dc84d43cc5327
MaxBouricius <55273830+MaxBouricius@users.noreply.github.com>
2022-12-15 21:48:49 +0100

    made it so that when you click the Activity label it also changes color

--- src/Components/Activity/Activity.js
+++ src/Components/Activity/Activity.js
@@ -4,6 +4,7 @@ import "./Activity.css";
 const Activity = ({ activity }) => {
     
     const [labelText, setlabelText] = useState('Vandaag')
+    const [labelStyle, setlabelStyle] = useState('activity__label--green')
     let labelChange = () => {
         switch(labelText){
             case "Vandaag":
@@ -16,12 +17,23 @@ const Activity = ({ activity }) => {
                 setlabelText("Vandaag");
                 break;
         }
+        switch(labelStyle){
+            case "activity__label--green":
+                setlabelStyle("activity__label--orange");
+                break
+            case "activity__label--orange":
+                setlabelStyle("activity__label--red");
+                break;
+            case "activity__label--red":
+                setlabelStyle("activity__label--green");
+                break;
+        }
     }
 
     return (
 
         <li className="activity">
-            <h3 className="activity__label" onClick={labelChange}>{labelText || "----"}</h3>
+            <h3 className={labelStyle} onClick={labelChange}>{labelText || "----"}</h3>
             <p className="activity__discription">{activity.description || "description"}</p>
         </li>
 
